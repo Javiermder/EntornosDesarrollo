0eff4672523de2bca1cd9b3e3900b3ad80b56f14
DavidJLopez12 <davidjlopezrom12@gmail.com>
2023-09-29 14:28:05 +0200

    proyecto added

--- src/es/marquesdecomares/daw/ed/HolaMundoJavierMartinez.java
+++ src/es/marquesdecomares/daw/ed/HolaMundoJavierMartinez.java
@@ -5,7 +5,7 @@ public class HolaMundoJavierMartinez {
 	public static void main(String[] args) {
 
 		System.out.println("Hola mundo");
-		System.out.println("soy david1");
+		System.out.println("soy david");
 	}
 
 }
